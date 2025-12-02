1d679aed3da2242999180ec65864f6ae5d54c4a6
my_name <erj.weltschmerz@gmail.com>
2025-12-02 17:08:28 +0500

    second

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,55 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHtml(w http.ResponseWriter, r *http.Request) {
+	file, err := os.ReadFile("../index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(file)
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	convertedData := service.TextOrMorse(data)
+	newFile, err := os.Create(time.Now().UTC().Format("20060102_150405"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer newFile.Close()
+	_, err = newFile.WriteString(convertedData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(convertedData))
+
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func TextOrMorse(data []byte) string {
+	isMorse := true
+	count := 0
+	if data == nil {
+		log.Fatal("Отсутствует data из file")
+	}
+	dataToString := string(data)
+
+	for _, v := range dataToString {
+		if v == '.' || v == '-' || v == ' ' {
+			count++
+		} else {
+			isMorse = false
+			break
+		}
+	}
+
+	if isMorse && count == len(dataToString) {
+		dataText := morse.ToText(dataToString)
+		fmt.Println(dataText)
+		return dataText
+	}
+
+	dataMorse := morse.ToMorse(dataToString)
+	fmt.Println(dataMorse)
+	return dataMorse
+}
